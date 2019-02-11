36a3d88f0581e3d621dce736e8fcc2445f91444c
Thomas Phillips <thomasphillips3@gmail.com>
2019-02-11 01:31:08 +0000

    Turns on the LED while the button is pressed

--- button.py
+++ button.py
@@ -2,15 +2,19 @@ import RPi.GPIO as GPIO
 import time
 
 inPin = 8
+ledPin = 7
+
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(inPin, GPIO.IN)
+GPIO.setup(ledPin, GPIO.OUT)
 
 while True:
     value = GPIO.input(inPin)
     if value:
         print("pressed")
+        GPIO.output(ledPin, GPIO.HIGH)
     else:
-        print ("not pressed")
+        GPIO.output(ledPin, GPIO.LOW)
     time.sleep(0.1)
 GPIO.cleanup()
